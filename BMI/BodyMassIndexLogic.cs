16398c53e977525163f70703f23e95835bedf278
Marius Brandt <x-e-r-x-e-s@web.de>
2021-02-18 20:22:10 +0100

    Projektdateien hinzufügen.

---
+++ BMI/BodyMassIndexLogic.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace BMI
+{
+    public class BodyMassIndexLogic
+    {
+        public event Action<string> ShowResult;
+        public event Action ShowFormatError;
+
+        public BodyMassIndexLogic()
+        {
+        }
+
+        private double CalculateBodyMassIndex(int size, int weight)
+        {
+            double sizeMeter = size / 100d;
+            return Math.Round(weight / sizeMeter / sizeMeter, 1);
+        }
+
+        private String DetermineCategory(double bmi)
+        {
+            if (bmi < 13)
+            {
+                return "hochgradiges Untergewicht Grad II";
+            }
+            else if ((bmi >= 13) && (bmi < 16))
+            {
+                return "hochgradiges Untergewicht Grad I";
+            }
+            else if ((bmi >= 16) && (bmi < 17))
+            {
+                return "mäßiges Untergewicht";
+            }
+            else if ((bmi >= 17) && (bmi < 18.5))
+            {
+                return "leichtes Untergewicht";
+            }
+            else if ((bmi >= 18.5) && (bmi < 25))
+            {
+                return "Normalgewicht";
+            }
+            else if ((bmi >= 25) && (bmi < 30))
+            {
+                return "Präadipositas";
+            }
+            else if ((bmi >= 30) && (bmi < 35))
+            {
+                return "Adipositas Grad I";
+            }
+            else if ((bmi >= 35) && (bmi < 40))
+            {
+                return "Adipositas Grad II";
+            }
+            else
+            {
+                return "Adipositas Grad III";
+            }
+        }
+
+        public void EvaluateData(int size, int weight)
+        {
+            if ((size <= 0) || (weight <= 0))
+            {
+                ShowFormatError();
+            }
+            else
+            {
+                double bmi = CalculateBodyMassIndex(size, weight);
+                string result = $"Dein BMI ist {bmi} ({DetermineCategory(bmi)})";
+
+                if ((bmi < 18.5) || (bmi >= 25))
+                {
+                    double goodBmi;
+                    int goodWeight = weight;
+
+                    // Underweight
+                    if (bmi < 18.5)
+                    {
+                        do
+                        {
+                            goodWeight++;
+                            goodBmi = CalculateBodyMassIndex(size, goodWeight);
+                        } while (goodBmi < 18.5);
+                    }
+
+                    // Overweight
+                    if (bmi >= 25)
+                    {
+                        do
+                        {
+                            goodWeight--;
+                            goodBmi = CalculateBodyMassIndex(size, goodWeight);
+                        } while (goodBmi >= 25);
+                    }
+
+                    result = $"{result}\nNormalgewicht beginnt bei {goodWeight} kg";
+                }
+                ShowResult(result);
+            }
+        }
+    }
+}
---
+++ BMI/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace BMI
+{
+    public partial class MainWindow : Window
+    {
+        private readonly BodyMassIndexLogic logic;
+        private readonly Regex regexOnlyNumbers;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            logic = new BodyMassIndexLogic();
+            logic.ShowFormatError += ShowFormatError;
+            logic.ShowResult += ShowResult;
+
+            regexOnlyNumbers = new Regex("[^0-9]+");
+        }
+
+        private void TextBoxInputValidation(object sender, TextCompositionEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text.Length >= 3)
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                e.Handled = regexOnlyNumbers.IsMatch(e.Text);
+            }
+        }
+
+        private void ButtonCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            PerformCalculation();
+        }
+
+        private void PerformCalculation()
+        {
+            try
+            {
+                logic.EvaluateData(Convert.ToInt32(textboxSize.Text), Convert.ToInt32(textboxWeight.Text));
+            }
+            catch (FormatException)
+            {
+                ShowFormatError();
+            }
+        }
+
+        private void ShowFormatError()
+        {
+            MessageBox.Show("Keine gültigen Eingaben!", "Fehler", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void ShowResult(string result)
+        {
+            labelResult.Content = result;
+        }
+
+        private void TextBoxKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                PerformCalculation();
+            }
+        }
+    }
+}
